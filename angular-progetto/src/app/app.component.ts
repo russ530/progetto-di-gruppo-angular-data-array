4cd3ce839606291ef37c47d41a7ebb21ddaf5fea
russ530 guevarra <guevarra.russel@iisgalvanimi.edu.it>
2025-04-07 19:26:23 +0000

    creazione componente product list

--- angular-progetto/src/app/app.component.ts
+++ angular-progetto/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponentComponent } from './product-list-component/product-list-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular-progetto/src/app/product-list-component/product-list-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list-component',
+  imports: [],
+  templateUrl: './product-list-component.component.html',
+  styleUrl: './product-list-component.component.css'
+})
+export class ProductListComponentComponent {
+
+}
