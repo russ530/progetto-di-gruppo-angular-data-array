271a3a4334108f2f9d44653566449e6f32259e00
russ530 guevarra <guevarra.russel@iisgalvanimi.edu.it>
2025-04-07 19:32:48 +0000

    compilazione del componente product list

--- angular-progetto/src/app/product-list-component/product-list-component.component.ts
+++ angular-progetto/src/app/product-list-component/product-list-component.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-list-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list-component.component.html',
   styleUrl: './product-list-component.component.css'
 })
 export class ProductListComponentComponent {
+  products = [
+    { name: 'Visore per Realtà Virtuale', price: 499, description: 'Esperienza immersiva in realtà virtuale per il gaming' },
+    { name: 'Caricatore Portatile', price: 39, description: 'Power bank compatto per la ricarica in movimento' },
+    { name: 'Scooter Elettrico', price: 350, description: 'Scooter elettrico ecologico per gli spostamenti quotidiani' },
+    { name: 'Hub Smart Home', price: 150, description: 'Centro di controllo per dispositivi smart' },
+    { name: 'TV 4K', price: 899, description: 'TV Ultra HD 4K con funzionalità smart' }
+    ];
+    selectedProduct = null;
 
+    selectProduct(product: any) {
+      this.selectedProduct = product;
+    }
 }
