390ca42d8352fd41912bc3150130347451de9696
lornsntg bho <buhaj.lorenzo@iisgalvanimi.edu.it>
2025-04-08 11:57:31 +0000

    compilazione e visualizzazione del componente product detail

--- angular-progetto/src/app/product-detail-component/product-detail-component.component.ts
+++ angular-progetto/src/app/product-detail-component/product-detail-component.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail-component.component.html',
   styleUrl: './product-detail-component.component.css'
 })
 export class ProductDetailComponentComponent {
-
+  @Input() product!: { name: string; price: number; description: string };
 }
